63f2fc1565d35a4004781ce400cf506e0b1fbe5a
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-12-29 14:24:10 +0530

    Fix Production Database

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,17 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: process.env.POSTGRES_DB_USER,
-  host: process.env.POSTGRES_DB_HOST,
-  database: process.env.POSTGRES_DB_NAME,
-  password: process.env.POSTGRES_DB_PASSWORD,
-  port: process.env.POSTGRES_DB_PORT,
-});
+let pool;
+if (process.env.NODE_ENV === "development") {
+  pool = new Pool({
+    user: process.env.POSTGRES_DB_USER,
+    host: process.env.POSTGRES_DB_HOST,
+    database: process.env.POSTGRES_DB_NAME,
+    password: process.env.POSTGRES_DB_PASSWORD,
+    port: process.env.POSTGRES_DB_PORT,
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+}
 
 module.exports = pool;
